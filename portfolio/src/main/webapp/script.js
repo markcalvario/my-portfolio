5e31230950e0a67e9a28644b42cdf652c95a2d82
Mark Calvario <mac2492@columbia.edu>
2021-03-03 21:03:06 +0000

    - add a js function that uses the fetch() function to get the text from the servlet - created a div tag in html so that the message that gets retrieved will go there

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,2 +1,10 @@
+async function fetchDataFromServelet () {
+    const response = await fetch("/hello");
+    const text = await response.text();
 
+    const fetchContainer = document.getElementById("fetch-container");
+    fetchContainer.innerHTML = text;
+    fetchContainer.classList.add("text-center");
+}
+fetchDataFromServelet();
 
