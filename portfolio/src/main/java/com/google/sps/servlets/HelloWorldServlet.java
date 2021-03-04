2a325c11ea1d45b92b0bc44f7f07b05ccb29f9d5
Mark Calvario <mac2492@columbia.edu>
2021-03-04 03:42:25 +0000

    - add fun facts to my portfolio by using the fetch() and to json() in javascript - modify my servlet file so that it returns an array of fun facts about me in json format

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,5 +1,7 @@
 package com.google.sps.servlets;
 
+import java.util.ArrayList;
+import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,10 +11,22 @@ import javax.servlet.http.HttpServletResponse;
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;");
+        //response.getWriter().println("<h1>Hello world!</h1>");
+        String messages = getMessages();
+        response.getWriter().println(messages);
+    }
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
+    public String getMessages(){
+        ArrayList<String> messages = new ArrayList<String>();
+        messages.add("I have a husky named Max.");
+        messages.add("I like to listen to salas and cumbias.");
+        messages.add("My family is from a small town in Puebla, Mexico called San Baltazar.");
+
+        Gson gson = new Gson();
+        String json = gson.toJson(messages);
+        return json;
+    }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,10 +1,20 @@
 async function fetchDataFromServelet () {
     const response = await fetch("/hello");
-    const text = await response.text();
+    const text = await response.json();
+    console.log(text)
 
-    const fetchContainer = document.getElementById("fetch-container");
-    fetchContainer.innerHTML = text;
-    fetchContainer.classList.add("text-center");
+    const funFactsContainer = document.getElementById("fun-facts-container");
+    const unorderedList = document.createElement('ul');
+    text.map((fact,index)=>{
+        const factElement = document.createElement("li");
+        factElement.innerText = fact;
+        unorderedList.append(factElement);
+    })
+    funFactsContainer.append(unorderedList);
+
+
+    //funFactsContainer.innerHTML = text;
+    //fetchContainer.classList.add("text-center");
 }
 fetchDataFromServelet();
 
